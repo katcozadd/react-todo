0d64631c65cfef7f58bcb240c3643a4369b085cf
katcozadd <krosecozadd@gmail.com>
2018-05-02 14:40:19 -0600

    routes created

---
+++ src/containers/TodosContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+  render() {
+    return (
+      <h2>
+        I am the todo container page
+      </h2>
+    );
+  }
+}
+
+export default Todos;
